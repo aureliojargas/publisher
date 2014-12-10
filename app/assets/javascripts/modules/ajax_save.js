bcabf32e52299af822769a9605427bacf2e686a3
Paul Hayes <fofr@users.noreply.github.com>
2014-12-10 13:49:07 +0000

    Try out a workflow message

--- app/assets/javascripts/modules/ajax_save.js
+++ app/assets/javascripts/modules/ajax_save.js
@@ -2,14 +2,27 @@
   "use strict";
   Modules.AjaxSave = function() {
     this.start = function(element) {
-      var url = element.data('url') || element.attr('action') + '.json';
+      var url = element.data('url') || element.attr('action') + '.json',
+          message = element.find('.js-status-message');
 
       element.on('click', '.js-save', save);
 
       function save(evt) {
         evt.preventDefault();
 
-        $.ajax({
+        message.addClass('workflow-message-saving');
+        message.text('Saving…');
+        setTimeout(function() {
+          message.addClass('workflow-message-saved').removeClass('workflow-message-saving');
+          message.text('Saved');
+
+          setTimeout(function() {
+            message.removeClass('workflow-message-saved');
+          }, 2000);
+
+        }, 2000);
+
+        /*$.ajax({
           url : url,
           type : 'POST',
           data : element.serialize(),
@@ -17,7 +30,7 @@
             console.log('success');
             console.log(response);
           }
-        });
+        });*/
       }
     }
   };
